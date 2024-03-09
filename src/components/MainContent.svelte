f1a83ad4683116213ce7ff199138fb583771195f
Al Hera Ahmad <alheraahmad0786@gmail.com>
2024-03-10 02:03:45 +0530

    tried adding main section

--- src/components/MainContent.svelte
+++ src/components/MainContent.svelte
@@ -1,6 +1,57 @@
 <script>
 
 </script>
-<main>
-    I will be the main content section
+<main class="flex flex-col items-start md:items-center mx-auto">
+    <div class="relative flex items-start ml-12 md:ml-20">
+        <!-- <div class="absolute">Dec 2023</div> -->
+        <div class="mr-8">
+            <img src="/assets/bar_plain.jpg" alt="plain bar">
+        </div> 
+        <div class="w-[65%] flex gap-12 pt-8">
+            <div class="max-w-full rounded-lg overflow-hidden shadow-lg">
+                <img class="w-full aspect-square object-cover h-36" src="/assets/blog_img_1.jpg" alt="blog 1">
+                <div class="px-8 py-6">
+                  <p class="text-sm">
+                    No amount of technology can convert a bad story into a good story.
+                  </p>
+                </div>
+            </div>
+            <div class="max-w-full rounded-lg overflow-hidden shadow-lg">
+                <img class="w-full aspect-square object-cover h-36" src="/assets/blog_img_2.jpg" alt="blog 2">
+                <div class="px-8 py-6">
+                  <p class="text-sm">
+                    Most people don't have original ideas. Here is how Sam Altman pushes himself to have unpopular ideas.                  </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="relative">
+        <div class="mr-8">
+            <img src="/assets/bar_arrow.jpg" alt="arrow bar">
+        </div> 
+        
+    </div>
+
+
+    <div class="relative flex items-start ml-12 md:ml-20 mb-80">
+        <div class="mr-8">
+            <img src="/assets/bar_plain.jpg" alt="plain bar">
+        </div> 
+        <div class="blogs w-[65%] flex gap-12 pt-8">
+            <div class="max-w-full rounded-lg overflow-hidden shadow-lg">
+                <img class="w-full aspect-square object-cover h-36" src="/assets/blog_img_3.jpg" alt="blog 3">
+                <div class="px-8 py-6">
+                  <p class="text-sm">Your biggest regrets at 80 will be acts of omission.</p>
+                </div>
+            </div>
+            <div class="max-w-full rounded-lg overflow-hidden shadow-lg">
+                <img class="w-full aspect-square object-cover h-36" src="/assets/blog_img_4.png" alt="blog 4">
+                <div class="px-8 py-6">
+                  <p class="text-sm">Your biggest regrets at 80 will be acts of omission.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="relative"></div>
 </main>
